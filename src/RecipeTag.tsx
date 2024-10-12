74090a94e314ed92369121922230af9c47def209
Steffi Eaint <t3moea00@students.oamk.fi>
2024-10-13 01:20:46 +0300

    Assignment done

---
+++ src/RecipeTag.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface IRecipeTagProps {
+    tagName: string;
+    onSelectTag: (tagName: string) => void;
+}
+
+export default function RecipeTag({onSelectTag, tagName}: IRecipeTagProps) {
+  return (
+    <div>
+        <li onClick={() => onSelectTag(tagName)}>{tagName}</li>
+    </div>
+  )
+}
